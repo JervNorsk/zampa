b8eeaea054cc8942d9b3255c85750de5e2efeae4
hersel91 <hersel1991@gmail.com>
2020-03-20 22:07:47 +0100

    add sample.config

---
+++ sample.config.py
@@ -0,0 +1,31 @@
+class Config(object):
+    #########################################################################
+    #                            GENERAL CONFIG                             #
+    #########################################################################
+    BOT_TOKEN = "INSERT TOKEN HERE"
+    BOT_USER = " @usernamebot"
+    BOT_NAME = "zampa"
+    AUTHOR = " FurryDen Project"
+    VERSION = " 15.9.9(Quetzal)"
+    SOURCE = "https://github.com/FurryDen/zampa"
+    #########################################################################
+    #                            TELEGRAM CONFIG                            #
+    #########################################################################
+    STAFF_GROUP = -123456789
+    ADMIN_ID = [123456789]
+    OWNER = [123456789]
+    LOG_CHANNEL = -123456789
+    #########################################################################
+    #                            API CONFIG                                 #
+    #########################################################################
+    YANDEX_API = 'INSERT TOKEN HERE, VISIT: https://tech.yandex.com/translate'
+    OPENWEATHER_API = 'INSERT TOKEN HERE, VISIT: https://openweathermap.org/api'
+    #########################################################################
+    #                            DATABASE CONFIG                            #
+    #########################################################################
+    DATABASE_CONFIG = {
+        'server': 'localhost',
+        'user': 'dbuser',
+        'password': 'password',
+        'name': 'dbname',
+        }
